ea05d81c9c62d320bbd57c14fc909b20f1951cca
UKS <skulptorrr@duck.com>
2024-12-20 01:25:20 +0200

    Levenstain now returns FuzzySrchResult

--- Sources/Ifrit/Levenstain/Levenstain.swift
+++ Sources/Ifrit/Levenstain/Levenstain.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 public class Levenstain {
-    public static func searchSync(_ text: String, in aList: [String]) -> [SrchDetails] {
+    public static func searchSync(_ text: String, in aList: [String]) -> [FuzzySrchResult] {
         let tmp = aList.indices
-            .map { idx -> SrchDetails in
+            .map { idx -> FuzzySrchResult in
                 let score = aList[idx].levenshteinDistanceScore(to: text, trimWhiteSpacesAndNewLines: true)
                 
-                return SrchDetails(Int(idx), 1 - score, [])
+                return FuzzySrchResult(Int(idx), 1 - score, [])
             }
         
         return tmp
@@ -15,14 +15,14 @@ public class Levenstain {
     
     public static func searchSync<T>(_ text: String,
                                      in aList: [T],
-                                     by keyPath: KeyPath<T, [FuseProp]>) -> [SrchDetails] where T: Searchable
+                                     by keyPath: KeyPath<T, [FuseProp]>) -> [FuzzySrchResult] where T: Searchable
     {
         let tmp = aList.enumerated()
-            .compactMap { (idx, item) -> SrchDetails?  in
+            .compactMap { (idx, item) -> FuzzySrchResult?  in
                 let allValues = item[keyPath: keyPath].map{ $0.value }
                 
                 if let score = searchSync(text, in: allValues).first?.diffScore {
-                    return SrchDetails(Int(idx), score, [] )
+                    return FuzzySrchResult(Int(idx), score, [] )
                 }
                 
                 return nil
